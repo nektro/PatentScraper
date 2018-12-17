e7176092c91a31588a966d6fcf655cfbc803d2a3
Meghan <nektromc@gmail.com>
2018-12-17 07:35:12 -0500

    Create main.go

---
+++ main.go
@@ -0,0 +1,83 @@
+// @author Meghan#2032 <https://nektro.net/> 2018
+//
+// go run main.go "C:/uspto/"
+//
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) <= 1 {
+		fmt.Println("Please pass a directory as a parameter")
+		return
+	}
+	os.Mkdir(os.Args[1], os.ModePerm)
+	folder := os.Args[1]
+	if !strings.HasSuffix(folder, "/") && !strings.HasSuffix(folder, "\\") {
+		folder += string(os.PathSeparator)
+	}
+
+	a := 0
+	b := 0
+	c := 0
+
+	for i := 0; i < 10000000; i++ {
+		saveFile(folder, i, a, b, c)
+		a++
+		if a == 99 {
+			a = 0
+			b++
+		}
+		if b == 999 {
+			b = 0
+			c++
+		}
+	}
+}
+
+func saveFile(folder string, i int, a int, b int, c int) {
+	x := padLeft(fmt.Sprintf("%d", a), 2, "0")
+	y := padLeft(fmt.Sprintf("%d", b), 3, "0")
+	z := padLeft(fmt.Sprintf("%d", c), 3, "0")
+	url := fmt.Sprintf("http://pimg-fpiw.uspto.gov/fdd/%s/%s/%s/0.pdf", x, y, z)
+	file := folder + padLeft(fmt.Sprintf("%d", i), 8, "0") + ".pdf"
+
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		resp, err := http.Get(url)
+		if err != nil {
+			return
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode == 200 {
+			body, _ := ioutil.ReadAll(resp.Body)
+			ioutil.WriteFile(file, body, os.ModePerm)
+			fmt.Println("+ Saving:  " + url)
+		} else {
+			fmt.Println("- Skipped: " + url)
+		}
+	} else {
+		fmt.Println("- Passed:  " + url)
+	}
+}
+
+func padLeft(_this string, desiredLength int, pad string) string {
+	if len(_this) >= desiredLength {
+		return _this
+	}
+	right := min(len(pad), desiredLength-len(_this))
+	padding := pad[0:right]
+	return padLeft(padding+_this, desiredLength, pad)
+}
+
+func min(x int, y int) int {
+	if x > y {
+		return y
+	}
+	return x
+}
